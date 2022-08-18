f066d456decacf8b33876d56860095a1f5b325b8
Roger Massana <rogermassana2002@gmail.com>
2022-08-18 12:57:09 +0200

    Create prepend method

--- linked_list.rb
+++ linked_list.rb
@@ -22,10 +22,21 @@ class LinkedList
     @head.next_node = Node.new
     @head.next_node.value=(value)
   end
+
+  def prepend(value)
+    if @head.value == nil
+      @head.value=(value)
+      return
+    end
+    nou_node = Node.new
+    nou_node.value=(value)
+    nou_node.next_node=(@head)
+    @head = nou_node
+  end
 end
  
 
 llista = LinkedList.new
 llista.append(3)
-llista.append(5)
+llista.prepend(5)
 p llista
